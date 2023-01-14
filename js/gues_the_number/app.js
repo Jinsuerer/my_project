01e88978a36a6704c4c691d6d54827bff45231f1
jin <hevi.viyan3@gmail.com>
2023-01-14 16:16:46 +0100

    color

--- js/gues_the_number/app.js
+++ js/gues_the_number/app.js
@@ -6,6 +6,10 @@ document.querySelector(".btn").addEventListener("click" , () => {
 
     const guessNumber = Number(document.querySelector(".input").value);
     // console.log(guessNumber);
+    const bodyBgColor = document.querySelector(".container");
+    const newH4 = document.querySelector("h4")
+    const newImg = document.querySelector("img")
+    
 
 if( randomNumber > guessNumber ){
     alert("Up")
@@ -16,6 +20,10 @@ if( randomNumber > guessNumber ){
     
 }else{
     alert("the spell is broken")
+    bodyBgColor.style.backgroundColor = "orange"
+    newH4.textContent = "clever witch"
+    newImg.style.borderRadius = "30px"
+    document.querySelector("img").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLdPNV6CvRQWP93CBCH6KtSN0NeVC_ZqsiM7RBsRnNCgztqZrkR3_i2AhNBuKsauVeBUA&usqp=CAU"
     
 }
 document.querySelector(".input").value = "";
